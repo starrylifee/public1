92aa2c611dd6567516a90fe69872a06d2b2c3450
starrylife <forinnocen@naver.com>
2024-07-21 08:47:18 +0900

    githundidden

--- pages/(공통-과학) 동물분석기.py
+++ pages/(공통-과학) 동물분석기.py
@@ -6,6 +6,19 @@ import toml
 from PIL import Image
 import io
 
+hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK{ display: none; }
+    #MainMenu{ visibility: hidden; }
+    footer { visibility: hidden; }
+    header { visibility: hidden; }
+    </style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 def to_markdown(text):
     text = text.replace('•', '*')
     return textwrap.indent(text, '> ', predicate=lambda _: True)
--- pages/(창도초) 쉬운 사회 조사학습.py
+++ pages/(창도초) 쉬운 사회 조사학습.py
@@ -9,6 +9,19 @@ import urllib.request
 import json
 from bs4 import BeautifulSoup
 
+hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK{ display: none; }
+    #MainMenu{ visibility: hidden; }
+    footer { visibility: hidden; }
+    header { visibility: hidden; }
+    </style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 # 프로젝트 루트 디렉토리 설정
 base_path = pathlib.Path(__file__).parent.parent
 
